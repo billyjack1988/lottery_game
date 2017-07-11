13d40efab84455c4c234a201d3112811d0356760
billyjack1988 <billyjacktattoos@gmail.com>
2017-07-11 12:05:09 -0500

    tryin to get a true or false answer

---
+++ Lottery.rb
@@ -0,0 +1,29 @@
+def grand_bash(my_number, bash_number)
+	matches  = []
+	bash_number.each do |n|
+		if n == my_number
+			matches << my_number
+		end 
+	end 
+	matches
+	p matches
+end 
+
+def close_num(my_number, bash_number)
+	if my_number[0] == bash_number[0]
+		counter += 1
+	end
+	if my_number[1] == bash_number[1]
+		counter += 1
+	end
+	if my_number[2] == bash_number[2]
+		counter += 1
+	end
+	if my_number[3] == bash_number[3]
+		counter += 1
+	end 
+	if my_number =~ bash_number
+		puts "true"
+	end
+end
+
---
+++ lottery_testing.rb
@@ -0,0 +1,30 @@
+require "minitest/autorun"
+require_relative "Lottery.rb"
+
+class Lottery_test < Minitest::Test
+
+	def test_return_empty_when_no_winning
+		my_number = "1234"
+		bash_number = []
+		assert_equal([], grand_bash(my_number, bash_number))
+	end
+
+	def test_no_matches
+		my_number = "1234"
+		bash_number = ["6666", "5764", "3436"]
+		assert_equal([], grand_bash(my_number, bash_number))
+	end
+
+	def test_return_single_match
+		my_number = "1234"
+		bash_number = ["3234", "5646", "0123"]
+		assert_equal([], grand_bash(my_number, bash_number))
+	end	
+
+	def test_to_see_if_one_number_matches
+		my_number = "1234"
+		bash_number = ["1239", "2323", "7773"] 
+		assert_equal([], close_num(my_number, bash_number))
+	end
+end
+
