23acdfaecdefb9262693f31bf5e7c1facadc8bd4
billyjack1988 <billyjacktattoos@gmail.com>
2017-07-11 12:20:39 -0500

    passed test

--- Lottery.rb
+++ Lottery.rb
@@ -4,23 +4,27 @@ def grand_bash(my_number, bash_number)
 		if n == my_number
 			matches << my_number
 		end 
-	end 
+	end
 	matches
-	p matches
 end 
 
 def close_num(my_number, bash_number)
+	counter = 0
 	if my_number[0] == bash_number[0]
 		counter += 1
+	else 0
 	end
 	if my_number[1] == bash_number[1]
 		counter += 1
+	else 0
 	end
 	if my_number[2] == bash_number[2]
 		counter += 1
+	else 0
 	end
 	if my_number[3] == bash_number[3]
 		counter += 1
+	else 0
 	end 
 	if counter == 3
 		true
--- lottery_testing.rb
+++ lottery_testing.rb
@@ -23,7 +23,7 @@ class Lottery_test < Minitest::Test
 
 	def test_to_see_if_one_number_matches
 		my_number = "1234"
-		bash_number = ["1239", "2323", "7773"] 
+		bash_number = "1239" 
 		assert_equal(true, close_num(my_number, bash_number))
 	end
 end
