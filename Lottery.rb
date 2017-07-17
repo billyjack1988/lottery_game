e0d9fc152a7d7a7fe5973e8d7f512b3ac7941596
billyjack1988 <billyjacktattoos@gmail.com>
2017-07-17 09:20:27 -0500

    made my meathod shorter

--- Lottery.rb
+++ Lottery.rb
@@ -10,105 +10,20 @@ end
 
 def close_num(my_number, bash_number)
 	counter = 0
-	if my_number[0] == bash_number[0]
-		counter += 1
-	else 0
-	end
-	if my_number[1] == bash_number[1]
-		counter += 1
-	else 0
-	end
-	if my_number[2] == bash_number[2]
-		counter += 1
-	else 0
-	end
-	if my_number[3] == bash_number[3]
-		counter += 1
-	else 0
-	end 
-	if counter == 3
-		true
-	end
-end
+	index = 0
 
-def bigger_num(my_number, bash_number)
-	counter = 0
-	if my_number[0] == bash_number[0]
-		counter += 1
-	else 0
-	end
-	if my_number[1] == bash_number[1]
-		counter += 1
-	else 0
-	end
-	if my_number[2] == bash_number[2]
-		counter += 1
-	else 0
-	end
-	if my_number[3] == bash_number[3]
-		counter += 1
-	else 0
-	end
-	if my_number[4] == bash_number[4]
-		counter += 1
-	else 0
-	end
-	if my_number[5] == bash_number[5]
-		counter += 1
-	else 0
-	end
-	
-	if my_number[6] == bash_number[6]
-		counter += 1
-	else 0
-	end
-
-	if my_number[7] == bash_number[7]
-		counter += 1
-	else 0
-	end
-	
-	if my_number[8] == bash_number[8]
-		counter += 1
-	else 0
-	end
-	
-	if counter == 8		
-		true
-	end
+		my_number.length.times do 
+			if my_number[index] == bash_number[index]
+				counter += 1		
+			end
+				index += 1
+		end 
+	# 	p "#{my_number.length - 1} my number here"
+	# p "#{counter} counter here "
+	 counter == my_number.length - 1
 end
 
 
-def five_numbers_are_right(my_number, bash_number)
-	counter = 0
-	if my_number[0] == bash_number[0]
-		counter += 1 
-	else 0
-	end 
-	if my_number[1] == bash_number[1]
-		counter += 1
-	else 0
-	end
-	if my_number[2] == bash_number[2]
-		counter += 1
-	else 0
-	end
-	if my_number[3] == bash_number[3]
-		counter += 1
-	else 0
-	end
-	if my_number[4] == bash_number[4]
-		counter += 1
-	else 0
-	end
-	if my_number[5] == bash_number[5]
-		counter += 1
-	else 0
-	end
-	if counter == 5 
-		true 
-	end
-end
 
 		
 
@@ -123,26 +38,26 @@ def winning_array(my_number, bash_number)
  	p a
 end
 
-def bigger_array(my_number, bash_number)
-	bash = []
-		bash_number.each do |bignum|
+# def bigger_array(my_number, bash_number)
+# 	bash = []
+# 		bash_number.each do |bignum|
 			
-		if bigger_num(my_number, bignum) == true
-				bash << bignum
-		end
-	end 
-	p bash
-end
+# 		if bigger_num(my_number, bignum) == true
+# 				bash << bignum
+# 		end
+# 	end 
+# 	p bash
+# end
 
-def five_numbers_array(my_number, bash_number)
-	c = []
-		bash_number.each do |five|
+# def five_numbers_array(my_number, bash_number)
+# 	c = []
+# 		bash_number.each do |five|
 			
-		if five_numbers_are_right(my_number, five) == true
-				c << five
-		end
-	end 
-	p c 
-end
+# 		if five_numbers_are_right(my_number, five) == true
+# 				c << five
+# 		end
+# 	end 
+# 	p c 
+# end
 
 	
--- lottery_testing.rb
+++ lottery_testing.rb
@@ -20,10 +20,15 @@ class Lottery_test < Minitest::Test
 		bash_number = ["3234", "5646", "1234"]
 		assert_equal(["1234"], grand_bash(my_number, bash_number))
 	end	
+	def test_to_see_if_false_works
+		my_number = "1234567"
+		bash_number = "1234567" 
+		assert_equal(false, close_num(my_number, bash_number))
+	end
 
-	def test_to_see_if_one_number_matches
-		my_number = "1234"
-		bash_number = "1239" 
+	def test_to_see_if_one_off
+		my_number = "1234567"
+		bash_number = "1234560" 
 		assert_equal(true, close_num(my_number, bash_number))
 	end
 
@@ -32,6 +37,7 @@ class Lottery_test < Minitest::Test
 		bash_number = ["1239", "2353", "3521"]
 		assert_equal(["1239"], winning_array(my_number, bash_number))
 	end
+	
 	def test_again
 		my_number = "1234"
 		bash_number = ["1239", "2353", "3521", "1323", "1233","6666"]
@@ -41,20 +47,20 @@ class Lottery_test < Minitest::Test
 	def test_for_larger_number
 		my_number = "123456789"
 		bash_number = ["123456783", "123456789", "123456788"]
-		assert_equal(["123456783", "123456788"], bigger_array(my_number, bash_number))
+		assert_equal(["123456783", "123456788"], winning_array(my_number, bash_number))
 	end
 
 	def test_for_larger_number_2
 		my_number = "123456789"
 		bash_number = ["123456788", "123", "234590", "bob", "2398", "123456787"]
-		assert_equal(["123456788", "123456787"], bigger_array(my_number, bash_number))
+		assert_equal(["123456788", "123456787"], winning_array(my_number, bash_number))
 	end 
 
 	
 	def test_for_five
-		my_number = "123456789"
+		my_number = "123457897"
 		bash_number = ["123457891", "888888888", "5678", "37737"]
-		assert_equal(["123457891"], five_numbers_array(my_number, bash_number))
+		assert_equal(["123457891"], winning_array(my_number, bash_number))
 	end
 end
 
